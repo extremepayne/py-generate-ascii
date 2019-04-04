14472e6b1e9827bdd3329649c95c5d60252bac58
extremepayne <harrison.payne@gmail.com>
2019-04-04 17:53:57 -0600

    Define a nested (2-D) list with same dimensions as image

--- main.py
+++ main.py
@@ -12,16 +12,32 @@ except ImportError:
     print("Do this in you command line: `pip install pillow`")
 
 
-user_input = input("Enter the path of your file (absolute or relative): ")
+user_input = input(
+    'Enter the path of your file\n\
+(absolute or relative path, type "demo" for a demo)\n'
+)
+
 if user_input == "demo":
     img = Image.open("images\python-pix-sm-transparent.png")
     print(img.format, img.size)
+    outpt_list = []
+    to_append = []
+    for column in range(img.size[0]):
+        to_append.append("")
+    for row in range(img.size[1]):
+        outpt_list.append(to_append)
+    print(outpt_list, len(outpt_list))
+
 elif ".png" in user_input:
     assert os.path.exists(user_input), "I did not find the file at, " + str(
         user_input
     )
     img = Image.open(user_input)
-    print(img.format, img.size)
+    for column in range(img.size[0]):
+        to_append.append("")
+    for row in range(img.size[1]):
+        outpt_list.append(to_append)
+
 else:
     print("File must be a png.\n\n\n")
 
