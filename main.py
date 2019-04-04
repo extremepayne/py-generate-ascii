887307c3f3497a9de79389b6d7d3395bd7c39237
extremepayne <harrison.payne@gmail.com>
2019-04-04 16:42:57 -0600

    Get a valid image from the user

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 """Module Docstring."""
+# pylint: disable=C0103
 
+import os
 import constants
 
+
 try:
-    import PIL
+    from PIL import Image
 except ImportError:
-    print("You need to install pillow for this project to work.")
+    print("You need to install pillow (6.x) for this project to work.")
     print("Do this in you command line: `pip install pillow`")
 
 
-print(constants.CHAR_DARKNESS)
+user_input = input("Enter the path of your file (absolute or relative): ")
+if user_input == "demo":
+    img = Image.open("images\python-pix-sm-transparent.png")
+    print(img.format, img.size)
+elif ".png" in user_input:
+    assert os.path.exists(user_input), "I did not find the file at, " + str(
+        user_input
+    )
+    img = Image.open(user_input)
+    print(img.format, img.size)
+else:
+    print("File must be a png.\n\n\n")
 
 print("This is a work in progress.")
