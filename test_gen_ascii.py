a70c8c32270f7d043254167739d4d4660236fcfb
extremepayne <harrison.payne@gmail.com>
2019-04-16 06:57:40 -0600

    improve test coverage

--- test_gen_ascii.py
+++ test_gen_ascii.py
@@ -5,3 +5,12 @@ import gen_ascii
 CHARS = gen_ascii.get_char_set(constants.CHAR_DARKNESS, constants.MAX_VAL)
 assert gen_ascii.generate_char(0, "r", CHARS) == " "
 assert gen_ascii.generate_char(3, "r", CHARS) == "`"
+assert gen_ascii.generate_char(21, "r", CHARS) in (
+    "a",
+    "n",
+    "s",
+    "y",
+    "z",
+    "*",
+)
+assert gen_ascii.generate_char(5, "l", CHARS) == "'"
