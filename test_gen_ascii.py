58039389d031c767d00d0e37a723eabd976672d3
extremepayne <46696318+extremepayne@users.noreply.github.com>
2019-04-16 10:12:38 -0600

    Update test_gen_ascii.py

--- test_gen_ascii.py
+++ test_gen_ascii.py
@@ -2,7 +2,9 @@ import constants
 import gen_ascii
 
 
-CHARS = gen_ascii.get_char_set(constants.CHAR_DARKNESS, constants.MAX_VAL)
+CHARS = gen_ascii.get_char_set(constants.CHAR_DARKNESS_TEXT_ONLY, constants.MAX_VAL)
+CHARS.update(gen_ascii.get_char_set(constants.CHAR_DARKNESS_NON_TEXT, constants.MAX_VAL)
+
 assert gen_ascii.generate_char(0, "r", CHARS) == " "
 assert gen_ascii.generate_char(3, "r", CHARS) == "`"
 assert gen_ascii.generate_char(21, "r", CHARS) in (
