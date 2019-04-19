17c1633ec0b92881c5574e6970bd7a33141d6fd1
extremepayne <46696318+extremepayne@users.noreply.github.com>
2019-04-19 08:50:46 -0600

    Test new API

--- test_gen_ascii.py
+++ test_gen_ascii.py
@@ -1,3 +1,5 @@
+import platform
+from PIL import Image
 import constants
 import gen_ascii
 
@@ -13,6 +15,9 @@ for smaller_list in CHARS:
         if character not in smaller_list:
             CHARS[i].append(character)
     i += 1
+LETTER_SCALE = constants.MAX_VAL - constants.MIN_VAL + 1
+GRAY_SCALE = 255 - 1 + 1
+SCALE = GRAY_SCALE / LETTER_SCALE
 
 assert gen_ascii.generate_char(0, "r", CHARS) == " "
 assert gen_ascii.generate_char(21, "r", CHARS) in (
@@ -27,3 +32,11 @@ assert gen_ascii.generate_char(21, "l", CHARS) == "a"
 assert gen_ascii.generate_char(3, "r", CHARS) in ("'", ".")
 assert gen_ascii.generate_char(32, "l", CHARS) == "H"
 assert gen_ascii.generate_char(34, "l", CHARS) == "@"
+
+
+my_os = platform.system()
+if my_os == "Windows":
+    img = Image.open("images\python.jpg")
+else:
+    img = Image.open("images/python.jpg")
+print(gen_ascii.create_ascii(img, CHARS, "l", 4, SCALE))
